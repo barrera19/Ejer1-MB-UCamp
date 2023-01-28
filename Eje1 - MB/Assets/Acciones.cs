f64294dbce6679e3d26d18deeb105451bf087866
barrera19 <barreraman19@gmail.com>
2023-01-28 14:43:13 -0500

    primeros cambios
    primeros cambios

--- Eje1 - MB/Assets/Acciones.cs
+++ Eje1 - MB/Assets/Acciones.cs
@@ -5,7 +5,7 @@ using TMPro;
 
 public class Acciones : MonoBehaviour
 {
-    public TextMeshProUGUI posibles, opreacion, resultado;
+    public TextMeshProUGUI posibles, opreacion, resultado, rrto;
     public TMP_InputField entrada1, entrada2;
     public int puntos = 0;
     public GameObject[] botones;
@@ -90,30 +90,160 @@ public class Acciones : MonoBehaviour
         botones[3].SetActive(false);
         botones[4].SetActive(true);
 
-        //Inicializar la Pregunta 3
-        posibles.text = "0, 0, 0, 0";
+        //Inicializar la Pregunta 5
+        posibles.text = "2, 8, 4, 3";
         opreacion.text = "+";
-        resultado.text = "0";
+        resultado.text = "10";
         entrada1.text = "";
         entrada2.text = "";
     }
-    //5 6 7 8 9 10
+  public void Pregunta5()
+    {
 
-    //Esconder entras y etiquetas
+        if (entrada1.text == "2" && entrada2.text == "8")
+        {
+            puntos++;
+        }
+        else if (entrada1.text == "8" && entrada2.text == "2")
+        {
+            puntos++;
+        }
+
+        botones[4].SetActive(false);
+        botones[5].SetActive(true);
+
+        //Inicializar la Pregunta 6
+        posibles.text = "1, 7, 5, 9";
+        opreacion.text = "+";
+        resultado.text = "14";
+        entrada1.text = "";
+        entrada2.text = "";
+    }
+
+      public void Pregunta6()
+    {
+
+        if (entrada1.text == "5" && entrada2.text == "9")
+        {
+            puntos++;
+        }
+        else if (entrada1.text == "9" && entrada2.text == "5")
+        {
+            puntos++;
+        }
+
+        botones[5].SetActive(false);
+        botones[6].SetActive(true);
+
+        //Inicializar la Pregunta 7
+        posibles.text = "8, 3, 4, 1";
+        opreacion.text = "+";
+        resultado.text = "5";
+        entrada1.text = "";
+        entrada2.text = "";
+    }
+
+      public void Pregunta7()
+    {
+
+        if (entrada1.text == "4" && entrada2.text == "1")
+        {
+            puntos++;
+        }
+        else if (entrada1.text == "1" && entrada2.text == "4")
+        {
+            puntos++;
+        }
+
+        botones[6].SetActive(false);
+        botones[7].SetActive(true);
+
+        //Inicializar la Pregunta 8
+        posibles.text = "10, 21, 9, 15";
+        opreacion.text = "+";
+        resultado.text = "30";
+        entrada1.text = "";
+        entrada2.text = "";
+    }
+
+      public void Pregunta8()
+    {
+
+        if (entrada1.text == "21" && entrada2.text == "9")
+        {
+            puntos++;
+        }
+        else if (entrada1.text == "9" && entrada2.text == "21")
+        {
+            puntos++;
+        }
+
+        botones[7].SetActive(false);
+        botones[8].SetActive(true);
+
+        //Inicializar la Pregunta 9
+        posibles.text = "4, 5, 7, 3";
+        opreacion.text = "+";
+        resultado.text = "9";
+        entrada1.text = "";
+        entrada2.text = "";
+    }
+      public void Pregunta9()
+    {
+
+        if (entrada1.text == "4" && entrada2.text == "5")
+        {
+            puntos++;
+        }
+        else if (entrada1.text == "5" && entrada2.text == "4")
+        {
+            puntos++;
+        }
+
+        botones[8].SetActive(false);
+        botones[9].SetActive(true);
+
+        //Inicializar la Pregunta 10
+        posibles.text = "12, 9, 3, 20";
+        opreacion.text = "+";
+        resultado.text = "29";
+        entrada1.text = "";
+        entrada2.text = "";
+    }
+      public void Pregunta10()
+    {
+
+        if (entrada1.text == "20" && entrada2.text == "9")
+        {
+            puntos++;
+        }
+        else if (entrada1.text == "9" && entrada2.text == "20")
+        {
+            puntos++;
+        }
+
+        botones[9].SetActive(false);
+        botones[10].SetActive(true);
+
+      //Esconder entras y etiquetas
+
+    }
+   
     //mostrar RRETO
     public void MostrarRetro()
     {
         if (puntos <= 6)
         {
             //RETRO propiedad TEXT pornerle el valor "------"
+            rrto.text = "Ups,Esfuérzate un poco más, intenta nuevamente para que obtengas más puntos";
         }
         else if(puntos <= 9)
         {
-
+            rrto.text = "¡Lo has hecho muy bien, pero puedes volverlo a intentar si deseas obtener un puntaje mayor";
         }
         else if(puntos == 10)
         {
-
+            rrto.text = "Wow, no te equivocaste, felicidades, lograste el puntaje máximo posible!";
         }
     }
 }
